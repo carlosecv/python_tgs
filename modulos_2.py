96aa4a605208324509deca7ebc51792220555c89
Carlos Contreras <carlosecv@realsystems.com.mx>
2026-02-10 15:29:57 -0600

    [IMP] Module import uses

---
+++ modulos_2.py
@@ -0,0 +1,4 @@
+#Por ejemplo, en lugar de importar toda la biblioteca math, puedes importar solo sqrt:
+from math import sqrt
+resultado = sqrt(25)
+print(resultado)
---
+++ modulos_3.py
@@ -0,0 +1,3 @@
+import math as mh # 'np' es un alias para 'numpy'
+arreglo = mh.sqrt(64)
+print(arreglo)
